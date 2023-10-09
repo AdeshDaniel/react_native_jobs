754e75558f406860e15c529a9addcf5a68bb71e3
AdeshDaniel <stockhutfxcompany@gmail.com>
2023-10-09 17:47:44 +0100

    made small dels

--- components/common/header/ScreenHeaderBtn.jsx
+++ components/common/header/ScreenHeaderBtn.jsx
@@ -2,16 +2,6 @@ import { Image, TouchableOpacity } from "react-native";
 
 import styles from "./screenheader.style";
 
-const ScreenHeaderBtn = ({ iconUrl, dimension, handlePress }) => {
-  return (
-    <TouchableOpacity style={styles.btnContainer} onPress={handlePress}>
-      <Image
-        source={iconUrl}
-        resizeMode='cover'
-        style={styles.btnImg(dimension)}
-      />
-    </TouchableOpacity>
-  );
-};
+const ScreenHeaderBtn = ({}) => {};
 
 export default ScreenHeaderBtn;
