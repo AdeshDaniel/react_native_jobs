2972b62ff0e6302e088e00e0dccada46d6dbadb7
AdeshDaniel <stockhutfxcompany@gmail.com>
2023-10-11 11:24:05 +0100

    completed the job details page

--- components/jobdetails/specifics/Specifics.jsx
+++ components/jobdetails/specifics/Specifics.jsx
@@ -2,6 +2,21 @@ import { View, Text } from "react-native";
 
 import styles from "./specifics.style";
 
-const Specifics = ({}) => {};
+const Specifics = ({ title, points }) => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>{title}:</Text>
+
+      <View style={styles.pointsContainer}>
+        {points.map((item, index) => (
+          <View style={styles.pointWrapper} key={item + index}>
+            <View style={styles.pointDot} />
+            <Text style={styles.pointText}>{item}</Text>
+          </View>
+        ))}
+      </View>
+    </View>
+  );
+};
 
 export default Specifics;
